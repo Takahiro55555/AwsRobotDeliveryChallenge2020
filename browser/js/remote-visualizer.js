ea1ac1d78a54056bc87f19bfba84fc9a5d0f5375
Takahiro55555 <tkhr.corei7@gmail.com>
2020-08-23 02:39:21 +0900

    update: AWS IoT の動作を確認。p5.js を導入。

--- browser/js/remote-visualizer.js
+++ browser/js/remote-visualizer.js
@@ -1,50 +1,84 @@
-const canvas = document.getElementById("canvas");  // canvas要素を取得
-const ctx = canvas.getContext("2d");  // コンテキストを取得
+const awsIot = require('aws-iot-device-sdk');
+const gm_subscribe_topic = iotclientId + "/ros_to_remote_console/obstacle_detector/merged_costmap/trimed";
 
-function start() {
-    // canvasのスタイルのサイズを取得
-    var canvas_width = canvas.clientWidth;
-    var canvas_height = canvas.clientHeight;
-
-    // canvasコンテキスト(?)のサイズとスタイルのサイズを統一
-    // こうしないと、図形描画時の縦横比がおかしくなる
-    canvas.width = canvas_width;
-    canvas.height = canvas_height;
+async function getCognitoCredentials() {
+    AWS.config.region = region;
+    var cognitoidentity = new AWS.CognitoIdentity();
+    var params = {
+        IdentityPoolId: PoolId
+    };
+    const identityId = await cognitoidentity.getId(params).promise();
+    const data = await cognitoidentity.getCredentialsForIdentity(identityId).promise();
+    var credentials = {
+        accessKey: data.Credentials.AccessKeyId,
+        secretKey: data.Credentials.SecretKey,
+        sessionToken: data.Credentials.SessionToken
+    };
+    return credentials;
 }
 
-var i = 0;
-function draw() {
-    var canvas_width = canvas.clientWidth;
-    var canvas_height = canvas.clientHeight;
-    var color = "#eaf4fc";
+async function setupAwsIot() {
+    const credentials = await getCognitoCredentials();
+    const deviceIot = awsIot.device({
+        region: region,
 
-    background(color);
+        // FIXME: Client ID をランダムにすると、複数コンソールの同時接続という問題を考えないといけなくなる...
+        clientId: iotclientId + "_" + String(Math.floor(Math.random() * 0x7ffffff)),
+        accessKeyId: credentials.accessKey,
+        secretKey: credentials.secretKey,
+        sessionToken: credentials.sessionToken,
+        protocol: 'wss',
+        port: 443,
+        host: iotendpoint
+    });
 
-    ctx.rect(10 + i, 10 + i, 100 + i, 100 + i);
-    ctx.stroke();
-    i += 2;
-    i = i % Math.floor(canvas_width / 2);
+    deviceIot.on('message', function (_topic, payload) {
+        const json = JSON.parse(payload.toString());
+        console.log(json);
+        isUpdated = true;
+    });
+
+    deviceIot.subscribe(gm_subscribe_topic, undefined, function (err, granted) {
+        if (err) {
+            console.log('subscribe error: ' + err);
+        } else {
+            console.log('subscribe success');
+        }
+    });
 }
 
-/**
- * canvasをリセットし、背景色を設定する
- * @param {背景に設定する色} color 
- */
-function background(color) {
-    var canvas_width = canvas.clientWidth;
-    var canvas_height = canvas.clientHeight;
-
-    ctx.beginPath();
-    ctx.fillStyle = color;
-    ctx.fillRect(0, 0, canvas_width, canvas_height);
-}
+setupAwsIot();
 
 
-/**** Processingぽくするための処理 ****/
-const fps = 30;
-start();
+const static_layer_sketch = function (p) {
+    p.setup = function () {
+        p.createCanvas(700, 700);
+        p.background(200);
+    };
 
-/**
- * FPSなどを設定し、draw関数を呼び出す
- */
-var intervalId = window.setInterval(draw, 1000 / fps);
+    let counter = 0;
+    p.draw = function () {
+        p.background(200);
+        p.ellipse(counter, counter, 100, 100);
+        counter++;
+        counter = counter % 700;
+    };
+};
+
+const dynmic_layer_sketch = function (p) {
+    p.setup = function () {
+        p.createCanvas(700, 700);
+        p.background(0, 0, 0, 0);
+    };
+
+    let counter = 0;
+    p.draw = function () {
+        p.setup();
+        p.ellipse(counter, 700 - counter, 100, 100);
+        counter++;
+        counter = counter % 700;
+    };
+};
+
+new p5(static_layer_sketch, "static-layer");
+new p5(dynmic_layer_sketch, "dynamic-layer");
